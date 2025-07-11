d86aa3344979e49e6d156ef3b40f2ffd35680dc8
lotaxgizmo <lotaoko2@gmail.com>
2025-07-11 09:59:26 +0100

    feat: update time interval to 1h and add Renko chart configuration with block size

--- config.js
+++ config.js
@@ -3,18 +3,19 @@
 // API settings
 export const api = 'bybit'; // 'binance' or 'bybit'
 // export const api = 'binance'; // 'binance' or 'bybit'
-export const time = '1m';
+export const time = '1h';
 export const symbol = 'BNBUSDT';
 // export const limit = 10080;
-export const limit = 1440;
+export const limit = 100;
 
-const hours = 0;
+
+const days = 6;
+const hours = days * 24;
 const minutes = hours * 60;
 export const delay = minutes;
 
-
-
-
+// Renko chart configuration
+export const renkoBlockSize = 3; // Size of each Renko block in USDT
 
 // Display settings
 export const showFullTimePeriod = true; // if false, only shows hours and minutes
@@ -25,8 +26,6 @@ export const highPercentile = 0.93;   // 93rd percentile
 export const lowPercentile = 0.10;    // 10th percentile
 export const topPercentile = 0.9955;
 
-export const pricePercentile = 0.9955;
+export const pricePercentile = 0;
 
 // export const lowPercentile = 0.2;
-
-
