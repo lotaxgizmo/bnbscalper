adf9c292d4301f7ba145079315acf6b5e3acc729
lotaxgizmo <lotaoko2@gmail.com>
2025-09-05 10:57:40 +0100

    dd

--- immediateAggregationBacktester.js
+++ immediateAggregationBacktester.js
@@ -21,8 +21,8 @@ const BACKTEST_CONFIG = {
     // maxCandles: 20160,          // 2 weeks of 1m candles for testing
     // maxCandles: 15840,          // 1 week of 1m candles for testing
     // maxCandles: 11520,          // 8 days of 1m candles for testing
-    maxCandles: 20160,          // 2 weeks of 1m candles for testing
-    // maxCandles: 10080,          // 1 week of 1m candles for testing
+    // maxCandles: 20160,          // 2 weeks of 1m candles for testing
+    maxCandles: 10080,          // 1 week of 1m candles for testing
     // maxCandles: 5760,          // 4 days of 1m candles for testing
     // maxCandles: 4320,          // 3 days of 1m candles for testing
     // maxCandles: 2880,          // 2 days of 1m candles for testing 
